fb7dba5564dc9261626ed7446bc6ecd23b938547
jfmmm <j.francoismorin@gmail.com>
2016-07-03 18:37:41 -0400

    Added entity name check on on_trigger_created_entity to prevent conflict with other mod

--- control.lua
+++ control.lua
@@ -1,11 +1,13 @@
 local function extinguish_fire(event)
-  local position = event.entity.position;
-  local area_start = {position.x - 1, position.y - 1}
-  local area_end = {position.x + 1, position.y + 1}
+  if event.entity.name == 'extinguisher-remnants' then
+    local position = event.entity.position;
+    local area_start = {position.x - 1, position.y - 1}
+    local area_end = {position.x + 1, position.y + 1}
 
-  for _, entity in pairs(game.surfaces['nauvis'].find_entities{area_start, area_end}) do
-    if entity.type == 'fire' then
-      entity.destroy()
+    for _, entity in pairs(game.surfaces['nauvis'].find_entities{area_start, area_end}) do
+      if entity.type == 'fire' then
+        entity.destroy()
+      end
     end
   end
 end
