09a63592c2705f3f8c01708254a2b1172e40262b
jfmmm <j.francoismorin@gmail.com>
2016-07-10 15:42:58 -0400

    Change to graphics, removed ground texture, added wind smoke.

--- prototypes/entity/extinguisher.lua
+++ prototypes/entity/extinguisher.lua
@@ -1,6 +1,40 @@
 require "util"
 
-local fire_damage_per_tick = 0 --45 / 60
+local function make_color(r_,g_,b_,a_)
+  return { r = r_ * a_, g = g_ * a_, b = b_ * a_, a = a_ }
+end
+
+data:extend({
+  {
+    type = "smoke",
+    name = "extinguisher-smoke",
+    flags = {"not-on-map"},
+    duration = 300,
+    fade_in_duration = 0,
+    fade_away_duration = 60,
+    spread_duration = 600,
+    spread_delay = 120,
+    start_scale = 1,
+  end_scale = 2,
+    color = make_color(1, 1, 1, 0.1),
+    cyclic = true,
+    affected_by_wind = true,
+    animation =
+    {
+      width = 152,
+      height = 120,
+      line_length = 5,
+      frame_count = 60,
+      axially_symmetrical = false,
+      direction_count = 1,
+      shift = {-0.53125, -0.4375},
+      priority = "high",
+      flags = { "compressed" },
+      animation_speed = 0.25,
+      filename = "__extinguisher__/graphics/entity/smoke/smoke.png"
+    }
+  }
+})
 
 data:extend({
   {
@@ -14,34 +48,35 @@ data:extend({
     selectable_in_game = false,
     subgroup = "remnants",
     order="d[remnants]-a[generic]-a[small]",
-    time_before_removed = 60 * 15, -- 15 minutes
-    final_render_layer = "remnants",
+    time_before_removed = 60 * 60 * 5, -- 15 minutes
+    final_render_layer = "lower-object",
     animation =
     {
-      {
-        width = 75,
-        height = 75,
-        frame_count = 1,
-        direction_count = 1,
-        filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-remnants-2.png"
-      },
-      {
-        width = 75,
-        height = 75,
-        frame_count = 1,
-        direction_count = 1,
-        y = 75,
-        filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-remnants-2.png"
-      },
-      {
-        width = 75,
-        height = 75,
-        frame_count = 1,
-        direction_count = 1,
-        y = 150,
-        filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-remnants-2.png"
-      }
-    }
+--      {
+--        width = 75,
+--        height = 75,
+--        frame_count = 1,
+--        direction_count = 1,
+--        filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-remnants-2.png"
+--      },
+--      {
+--        width = 75,
+--        height = 75,
+--        frame_count = 1,
+--        direction_count = 1,
+--        y = 75,
+--        filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-remnants-2.png"
+--      },
+--      {
+--        width = 75,
+--        height = 75,
+--        frame_count = 1,
+--        direction_count = 1,
+--        y = 150,
+--        filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-remnants-2.png"
+--      }
+    },
+
   },
 })
 
@@ -58,15 +93,25 @@ data:extend({
       }
     },
 
+    smoke_sources =
+    {
+      {
+        name = "extinguisher-smoke",
+        frequency = 0.15,
+        position = {0.0, 0},
+        starting_frame_deviation = 60
+      }
+    },
+
     particle_buffer_size = 65,
     particle_spawn_interval = 1,
     particle_spawn_timeout = 1,
     particle_vertical_acceleration = 0.005 * 0.6,
     particle_horizontal_speed = 0.25,
     particle_horizontal_speed_deviation = 0.0035,
-    particle_start_alpha = 0.5,
+    particle_start_alpha = 0.8,
     particle_end_alpha = 1,
-    particle_start_scale = 0.2,
+    particle_start_scale = 0.1,
     particle_loop_frame_count = 3,
     particle_fade_out_threshold = 0.9,
     particle_loop_exit_threshold = 0.25,
@@ -92,7 +137,7 @@ data:extend({
     spine_animation =
     {
       filename = "__extinguisher__/graphics/entity/extinguisher-stream/extinguisher-stream-spine.png",
-      blend_mode = "additive",
+      blend_mode = "additive-soft",
       --tint = {r=1, g=1, b=1, a=0.5},
       line_length = 4,
       width = 32,
@@ -100,8 +145,8 @@ data:extend({
       frame_count = 32,
       axially_symmetrical = false,
       direction_count = 1,
-      animation_speed = 2,
-      scale = 0.75,
+      animation_speed = 1,
+      scale = 0.60,
       shift = {0, 0},
     },
 
@@ -113,7 +158,7 @@ data:extend({
       height = 64,
       frame_count = 32,
       line_length = 8,
-      scale = 1,
+      scale = 1.5,
     },
   }
 })
